aa41882c3acf1e5109aa21277ea821b38a27198c
gabizinha <gabrielamiyazaki55@gmail.com>
2023-10-31 07:27:32 -0300

    correção da exportação do app

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,10 @@ import express, { Application } from "express";
 import { handleErrors } from "./middlewares/handleErrors.middleware";
 import { routes } from "./routes/index.routes";
 
-export const app: Application = express()
+const app: Application = express()
 
 app.use(express.json())
 app.use('/', routes)
 app.use(handleErrors)
+
+export default app 
